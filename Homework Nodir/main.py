74277f50fbe68ff5e71d7b7a8ff37a987e5ddc3d
Nodirjon Xojiakbarov <nodirjonxojiakbarov19@gmail.com>
2023-04-08 15:31:42 +0400

    Homework

---
+++ Homework Nodir/main.py
@@ -0,0 +1,35 @@
+# Урок №1, задание №1
+
+# first_name = input('Введите своё имя: ')
+# Second_name = input('Введите своё фамилию: ')
+# Name = first_name + ' ' + Second_name
+# print("hello", (Name))
+
+# Урок №1, задание №2
+
+# number1 = int(input('Введите первое число: '))
+# number2 = int(input('Введите второе число: '))
+# number3 = int(input('Введите третье число: '))
+# number4 = number1 + number2 + number3
+# print(f"Сумма чисел {number1}, {number2} и {number3} равно {number4}")
+
+# Урок №1, задание №4
+
+# a = 1
+#
+#
+#
+# numw = int(input('Ведите число: '))
+# print('Первое ----->', numw + a,' Второе ----> ',numw - a)
+
+# Урок №1, задание №5
+
+#print('Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\t How I wonder what you are')
+
+# Урок №1, задание №6
+
+# totalMoney = input("У меня есть денег ")
+# quantity = input("Я покупаю ")
+# price = input("За ")
+
+# 3 и 6 не понял
